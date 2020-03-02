84aab0c00bbc22dd81f450b5f797ec7c8a0a9996
Aleksander Wardzinski <aleks.wardzinski@gmail.com>
2020-03-02 23:58:00 +0100

    Add Initial Project

---
+++ src/test/java/com/alward/trainings/sfgpetclinic/SfgPetClinicApplicationTests.java
@@ -0,0 +1,13 @@
+package com.alward.trainings.sfgpetclinic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SfgPetClinicApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
