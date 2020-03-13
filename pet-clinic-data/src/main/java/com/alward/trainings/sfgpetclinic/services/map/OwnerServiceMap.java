5befa18c2beef07b17e0e49689855f2351fea682
Aleksander Wardzinski <aleks.wardzinski@gmail.com>
2020-03-13 13:24:49 +0100

    Adding Spring config. Closes #21.

--- pet-clinic-data/src/main/java/com/alward/trainings/sfgpetclinic/services/map/OwnerServiceMap.java
+++ pet-clinic-data/src/main/java/com/alward/trainings/sfgpetclinic/services/map/OwnerServiceMap.java
@@ -2,9 +2,11 @@ package com.alward.trainings.sfgpetclinic.services.map;
 
 import com.alward.trainings.sfgpetclinic.model.Owner;
 import com.alward.trainings.sfgpetclinic.services.OwnerService;
+import org.springframework.stereotype.Service;
 
 import java.util.Set;
 
+@Service
 public class OwnerServiceMap extends AbstractMapService<Owner, Long> implements OwnerService {
     @Override
     public Set<Owner> findAll() {
--- pet-clinic-data/src/main/java/com/alward/trainings/sfgpetclinic/services/map/PetServiceMap.java
+++ pet-clinic-data/src/main/java/com/alward/trainings/sfgpetclinic/services/map/PetServiceMap.java
@@ -2,9 +2,11 @@ package com.alward.trainings.sfgpetclinic.services.map;
 
 import com.alward.trainings.sfgpetclinic.model.Pet;
 import com.alward.trainings.sfgpetclinic.services.PetService;
+import org.springframework.stereotype.Service;
 
 import java.util.Set;
 
+@Service
 public class PetServiceMap extends AbstractMapService<Pet, Long> implements PetService {
     @Override
     public Set<Pet> findAll() {
