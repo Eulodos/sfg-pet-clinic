9b8a7edaf526a9e7b842dbc564a847991992e15a
Aleksander Wardzinski <aleks.wardzinski@gmail.com>
2020-03-13 13:04:10 +0100

    Adding bootstrap for data, refactoring Map Services. Closes #13.

--- pet-clinic-data/src/main/java/com/alward/trainings/sfgpetclinic/services/map/VetServiceMap.java
+++ pet-clinic-data/src/main/java/com/alward/trainings/sfgpetclinic/services/map/VetServiceMap.java
@@ -1,11 +1,11 @@
 package com.alward.trainings.sfgpetclinic.services.map;
 
 import com.alward.trainings.sfgpetclinic.model.Vet;
-import com.alward.trainings.sfgpetclinic.services.CrudService;
+import com.alward.trainings.sfgpetclinic.services.VetService;
 
 import java.util.Set;
 
-public class VetServiceMap extends AbstractMapService<Vet, Long> implements CrudService<Vet, Long> {
+public class VetServiceMap extends AbstractMapService<Vet, Long> implements VetService {
     @Override
     public Set<Vet> findAll() {
         return super.findAll();
