caddf4b95880f2a71303da40a195c664292b6635
Aleksander Wardzinski <aleks.wardzinski@gmail.com>
2020-03-22 14:21:42 +0100

    Created OwnerSpringDataJpaService. Closes #39.

--- pet-clinic-data/src/main/java/com/alward/trainings/sfgpetclinic/repositories/OwnerRepository.java
+++ pet-clinic-data/src/main/java/com/alward/trainings/sfgpetclinic/repositories/OwnerRepository.java
@@ -3,5 +3,9 @@ package com.alward.trainings.sfgpetclinic.repositories;
 import com.alward.trainings.sfgpetclinic.model.Owner;
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.Optional;
+
 public interface OwnerRepository extends CrudRepository<Owner, Long> {
+
+    Optional<Owner> findByLastName(String lastName);
 }
